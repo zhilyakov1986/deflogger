ec439907ecaa0f40f32c6a3c6d454951c9c05df7
zhilyakov1986 <zhilyakov1986@gmail.com>
2018-05-14 21:23:58 -0400

    service and interfade

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Log } from '../../models/Log';
+
 @Component({
   selector: 'app-logs',
   templateUrl: './logs.component.html',
   styleUrls: ['./logs.component.css']
 })
 export class LogsComponent implements OnInit {
+  logs: Log[];
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
---
+++ src/app/services/log.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+import { Log } from '../models/Log';
+
+@Injectable()
+export class LogService {
+  logs: Log[];
+
+  constructor() {
+    this.logs = [
+      { id: '1', text: 'Generated components', date: new Date('12/26/2017 12:54:23') },
+      { id: '2', text: 'Added Bootstrap', date: new Date('12/27/2017 9:54:23') },
+      { id: '3', text: 'Added logs component', date: new Date('12/27/2017 12:00:03') },
+    ];
+  }
+
+}
